82ab42e6dbd1374f9e10b81dd467431c98b0ac1f
rin-k645 <mwtgal20@gmail.com>
2022-03-17 14:11:06 +0900

    3월 17일 문제 풀이

--- src/DP/WineTasting.java
+++ src/DP/WineTasting.java
@@ -4,29 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class StairClimb {
+public class WineTasting {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int n = Integer.parseInt(br.readLine());
 		
-		int[] stair = new int[n + 1];
+		int[] glass = new int[n + 1];
 		
 		for(int i = 1; i <= n; i++) {
-			stair[i] = Integer.parseInt(br.readLine());
+			glass[i] = Integer.parseInt(br.readLine());
 		}
 		
 		int[] dp = new int[n + 1];
 		
-		dp[1] = stair[1];
-	
+		dp[1] = glass[1];
+		
 		if(n >= 2) {
-			dp[2] = stair[1] + stair[2];
+			dp[2] = glass[1] + glass[2];
 		}
 		
 		for(int i = 3; i <= n; i++) {
-			dp[i] = Math.max(dp[i - 3] + stair[i - 1], dp[i - 2]) + stair[i];
+			dp[i] = Math.max(dp[i - 1], Math.max(glass[i - 1] + dp[i - 3], dp[i - 2]) + glass[i]);
 		}
 		
 		System.out.println(dp[n]);
