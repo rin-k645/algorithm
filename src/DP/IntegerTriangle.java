f14f65944bd677f67d8c23c9e27c47363d1303ce
rin-k645 <mwtgal20@gmail.com>
2022-03-22 01:17:02 +0900

    3월 21일 문제 풀이

---
+++ src/DP/IntegerTriangle.java
@@ -0,0 +1,35 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class IntegerTriangle {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[][] triangle = new int[n][];
+		
+		for(int i = 0; i < n; i++) {
+			triangle[i] = new int[i + 1];
+			
+			String[] input = br.readLine().split(" ");
+			for(int j = 0; j <= i; j++) {
+				triangle[i][j] = Integer.parseInt(input[j]);
+			}
+		}
+		
+		for(int i = n - 1; i > 0; i--) {
+			for(int j = 0; j < i; j++) {
+				triangle[i - 1][j] += Math.max(triangle[i][j], triangle[i][j + 1]);
+			}
+			
+		}
+		
+		System.out.println(triangle[0][0]);
+	}
+
+}
