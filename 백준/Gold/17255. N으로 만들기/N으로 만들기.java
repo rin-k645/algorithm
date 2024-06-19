a556825b11b8e0bf3ea0eb49a0227c07933c599f
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2024-06-19 14:58:00 +0900

    [Gold IV] Title: N으로 만들기, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Gold/17255. N으로 만들기/N으로 만들기.java
+++ 백준/Gold/17255. N으로 만들기/N으로 만들기.java
@@ -1,36 +1,32 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 	static int count;
-	static HashSet<String> set;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		String N = br.readLine();
 		
-		set = new HashSet<>();
+		recursive(N);
 		
-		recursive(N, "");
-		
-		System.out.println(set.size());
+		System.out.println(count);
 	}
 	
-	public static void recursive(String num, String pro) { //정수, 과정 저장 
+	public static void recursive(String num) {
 		if(num.length() == 1) {
-			if(!set.contains(pro)) {
-				count++;
-				set.add(pro);
-			}
+			count++;
 			return;
 		}
 		
 		String frontRemoved = num.substring(1, num.length());
 		String backRemoved = num.substring(0, num.length() - 1);
 		
-		recursive(frontRemoved, pro.concat(frontRemoved));
-		recursive(backRemoved, pro.concat(backRemoved));
+		recursive(frontRemoved);
+		
+		if(frontRemoved.equals(backRemoved)) return;
+		
+		recursive(backRemoved);
 	}
 
 }
