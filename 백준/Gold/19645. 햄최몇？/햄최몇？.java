d00c98fe3fce407766f187c2f6cbe712ded7339e
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2024-07-05 22:22:05 +0900

    [Gold I] Title: 햄최몇?, Time: 752 ms, Memory: 24652 KB -BaekjoonHub

---
+++ 백준/Gold/19645. 햄최몇？/햄최몇？.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		int sum = 0;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			sum += arr[i];
+		}
+		
+		boolean[][] dp = new boolean[2501][2501]; //관우, 철환의 효용이 가능한지 
+		dp[0][0] = true;
+		
+		
+		for(int i = 0; i < N; i++) { //모든 햄버거 탐색 
+			for(int j = sum; j >= 0; j--) {
+				for(int k = sum; k >= 0; k--) {
+					if(dp[j][k]) { //이전이 가능한 조합이면, 현재 햄버거 먹은 조합도 가능 
+	                    dp[j + arr[i]][k] = true; //관우 먹음 
+	                    dp[j][k + arr[i]] = true; //철환 먹음 
+	                }
+				}
+			}
+		}
+		
+		//효용 최대 구하기
+		int max = 0;
+		for(int i = 0; i <= sum; i++) {
+	        for(int j = 0; j <= sum; j++) {
+	            if(dp[i][j]) { //가능한 조합
+	            	int gilwon = Math.min(sum - i - j, Math.min(i, j)); //가장 낮은 효용이 길원 
+	            	max = Math.max(max, gilwon);
+	            }
+	        }
+	    }
+		
+		System.out.println(max);
+	}
+
+}
