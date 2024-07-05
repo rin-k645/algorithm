e78982382ce683a2aab625854c866844254187c5
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2024-07-05 22:23:41 +0900

    [Gold I] Title: 햄최몇?, Time: 304 ms, Memory: 24652 KB -BaekjoonHub

--- 백준/Gold/19645. 햄최몇？/햄최몇？.java
+++ 백준/Gold/19645. 햄최몇？/햄최몇？.java
@@ -8,20 +8,20 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		
 		int[] arr = new int[N];
-		int sum = 0;
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		for(int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			sum += arr[i];
 		}
 		
 		boolean[][] dp = new boolean[2501][2501]; //관우, 철환의 효용이 가능한지 
 		dp[0][0] = true;
 		
-		
-		for(int i = 0; i < N; i++) { //모든 햄버거 탐색 
+		int sum = 0;
+		for(int i = 0; i < N; i++) { //모든 햄버거 탐색
+			sum += arr[i];
+			
 			for(int j = sum; j >= 0; j--) {
 				for(int k = sum; k >= 0; k--) {
 					if(dp[j][k]) { //이전이 가능한 조합이면, 현재 햄버거 먹은 조합도 가능 
