0c9cd463dae71c095b8da1db47914e31f2bbaa1c
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2024-09-26 17:52:20 +0900

    [level 3] Title: 단어 변환, Time: 0.52 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,35 +1,41 @@
-class Solution {
-    static boolean[] visited;
-    static int answer = 0;
-    
-    public int solution(String begin, String target, String[] words) {
-        visited = new boolean[words.length];
+import java.util.*;
 
-        dfs(begin, target, words, 0);
-        return answer;
+class Solution {
+    public int solution(String begin, String target, String[] words) {
+        Queue<String> queue = new LinkedList<>();
+        boolean[] visited = new boolean[words.length];
+        
+        queue.add(begin);
+        
+        int answer = 0;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            
+            for(int s = 0; s < size; s++) {
+                String str = queue.poll();
+            
+                if(str.equals(target)) return answer;
+                
+                for(int i = 0; i < words.length; i++) {
+                    if(!visited[i] && check(str, words[i])) {
+                        queue.add(words[i]);
+                        visited[i] = true;
+                    }
+                }
+            }
+            answer++;
+        }
+        
+        return 0;
     }
     
-    public static void dfs(String begin, String target, String[] words, int count) {
-		if(begin.equals(target)) { //종료 조건
-			answer = count;
-			return;
-		}
-		
-		for(int i = 0; i < words.length; i++) {
-			if(!visited[i]) { //방문하지 않은 단어면
-				int same = 0;
-				for(int j = 0; j < begin.length(); j++) { //몇 글자 같은지 구하기
-					if(begin.charAt(j) == words[i].charAt(j)) {
-						same++;
-					}
-				}
-				
-				if(same == begin.length() -1) { //한 글자 빼고 같으면
-					visited[i] = true; //방문했다하고
-					dfs(words[i], target, words, count + 1); //그 단어 기준으로 탐색
-					visited[i] = false; //탐색 끝나면 원래대로 되돌리기
-				}
-			}
-		}
-	}
+    private boolean check(String s1, String s2) {
+        int count = 0;
+        
+        for(int i = 0; i < s1.length(); i++){
+            if(s1.charAt(i) != s2.charAt(i)) count++;
+        }
+        
+        return count == 1 ? true : false;
+    }
 }
