6580503f45ad1c3ee42eae07eac895551d79ba7b
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2024-12-11 12:20:15 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16444 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int left = 0;
+		int right = N - 1;
+		int answer = 0;
+		
+		while(left < right) {
+			int sum = arr[left] + arr[right];
+			
+			if(sum == M) answer++;
+			
+			if(sum <= M) left++;
+			else right--;
+		}
+		
+		System.out.println(answer);
+	}
+
+}
