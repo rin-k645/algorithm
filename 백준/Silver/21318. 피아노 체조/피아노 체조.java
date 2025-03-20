44c3bccbeebbe00bd9892b12d940c89732391a26
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2025-03-20 15:49:58 +0900

    [Silver I] Title: 피아노 체조, Time: 752 ms, Memory: 63608 KB -BaekjoonHub

---
+++ 백준/Silver/21318. 피아노 체조/피아노 체조.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N + 1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 1; i < N + 1; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[] dp = new int[N + 1]; //i번째까지 실수하는 곡 개수
+		for(int i = 1; i <= N - 1; i++) {
+			if(arr[i] > arr[i + 1]) { //실수하게 되는 경우
+				dp[i] = dp[i - 1] + 1;
+			} else {
+				dp[i] = dp[i - 1];
+			}
+		}
+		
+		StringBuilder sb = new  StringBuilder();
+		int Q = Integer.parseInt(br.readLine());
+		for(int i = 0; i < Q; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			sb.append(dp[y - 1] - dp[x - 1] + "\n");
+		}
+		System.out.println(sb);
+	}
+
+}
