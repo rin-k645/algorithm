58e90aa38c4a2a25ab6dd6af2db53062fb12d864
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2025-05-09 20:17:26 +0900

    [Gold III] Title: 싸지방에 간 준하, Time: 888 ms, Memory: 63280 KB -BaekjoonHub

---
+++ 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[N][2];
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		// 시작 시간, 끝나는 시간 오름차순 정렬
+		Arrays.sort(arr, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]);
+
+		// (자리 번호, 끝나는 시간)이 삽입되어 끝나는 시간, 번호 작은 순 오름차순 정렬되는 우선순위 큐
+		PriorityQueue<int[]> pq1 = new PriorityQueue<>((o1, o2) -> o1[1] == o2[1] ? o1[0] - o2[0] : o1[1] - o2[1]);
+		
+		HashMap<Integer, Integer> map = new HashMap<>(); // 번호, 사람 수 기록
+		int idx = 0; // 자리 번호
+		
+		PriorityQueue<Integer> pq2 = new PriorityQueue<>(); // 자리가 난 큐
+		
+		for(int i = 0; i < N; i++) {
+//			System.out.println(arr[i][0] + ", " + arr[i][1] + " --------");
+			
+			while(!pq1.isEmpty() && arr[i][0] >= pq1.peek()[1]) {
+				int[] p  = pq1.poll();
+				pq2.add(p[0]);
+//				System.out.println(p[0] + " 컴퓨터에 자리 남");
+			}
+			
+			if(pq2.isEmpty()) { // 새로운 컴퓨터 필요
+				pq1.add(new int[]{++idx, arr[i][1]});
+				map.put(idx, 1);
+//				System.out.println("새 컴퓨터 추가: " + idx);
+			} else { // 기존 자리 사용
+				int p = pq2.poll();
+				pq1.add(new int[]{p, arr[i][1]});
+				map.put(p, map.get(p) + 1);
+//				System.out.println(p + " 컴퓨터에 들어감");
+			}
+		}
+		
+		// 출력
+		System.out.println(idx);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int key : map.keySet()) {
+			sb.append(map.get(key) + " ");
+		}
+		System.out.println(sb);
+	}
+
+}
