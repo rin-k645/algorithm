a6099aa7358c1eb14bb1e3155c523073f72e32f7
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2025-10-26 21:48:12 +0900

    [Gold II] Title: 대표 선수, Time: 1304 ms, Memory: 131208 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.java
+++ 백준/Gold/2461. 대표 선수/대표 선수.java
@@ -11,7 +11,6 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken()); // 각 학급의 학생 수
 		
 		int[][] arr = new int[N][M]; // 능력치
-		int[] pointer = new int[N]; // 포인터 위치 기록
 		
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -23,25 +22,34 @@ public class Main {
 		}
 		
 		// 구현
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[0] - o2[0]); // 능력치, 학급명, 학급 내 번호
+        int max = 0;
 		int answer = 1_000_000_000; // 최대와 최소 차이
 		
-		int j = 0;
-		int idx = 0;
-		while(j < M) {
-			int min = 1_000_000_000;
-			int max = 0;
-			
-			for(int i = 0; i < N; i++) {
-				if(min > arr[i][pointer[i]]) {
-					min = arr[i][pointer[i]];
-					idx = i;
-				}
-				max = Math.max(max, arr[i][pointer[i]]);
-			}
-			
-			answer = Math.min(answer, max - min);
-			j = ++pointer[idx];
-		}
+		for (int i = 0; i < N; i++) {
+            pq.offer(new int[]{arr[i][0], i, 0});
+            max = Math.max(max, arr[i][0]);
+        }
+		
+		while (true) {
+			// 능력치가 최소인 학생 뽑기
+            int[] p = pq.poll();
+            int min = p[0];
+            int i = p[1];
+            int j = p[2];
+
+            // 갱신
+            answer = Math.min(answer, max - min);
+            j++;
+            
+            if(j == M) {
+            	break;
+            }
+            
+            // 같은 반 다음 학생 넣기
+            pq.offer(new int[]{arr[i][j], i, j});
+            max = Math.max(max, arr[i][j]);
+        }
 		
 		// 출력
 		System.out.println(answer);
