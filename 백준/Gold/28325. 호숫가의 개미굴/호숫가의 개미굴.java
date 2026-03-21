37a1d8ef85c4fa39e2e3e52012288a0f9356837a
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2026-03-21 21:21:53 +0900

    [Gold IV] Title: 호숫가의 개미굴, Time: 368 ms, Memory: 44432 KB, Score: 80 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.java
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.java
@@ -9,48 +9,46 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		
 		long[] arr = new long[N + 1];
-		long sum = 0;
+		long sum = 0; // 쪽방 합
 	
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
 			arr[i] = Long.parseLong(st.nextToken());
+			// 쪽방에 개미를 넣는 것이 최대
 			sum += arr[i];
 		}
 		
 		// 구현
+		// 쪽방에 개미가 없는 경우
 		if(sum == 0){
-            System.out.println((N + 1) / 2);
+            System.out.println(N / 2);
             return;
         }
 		
+		// 나머지 경우
 		long answer = sum;
         List<Integer> list = new ArrayList<>();
 
-        int i = 0;
-        while(i < N) {
-            if(arr[i] != 0) {
-                i++;
-                continue;
+        // 0이 아닌 구간 찾기
+        int start = 0;
+        for(int i = 0; i < N; i++) {
+            if(arr[i] > 0) {
+                start = i;
+                break;
             }
-
-            int len = 0;
-            while(i < N && arr[i] == 0) {
+        }
+        
+        int len = 0; // 연속된 0 구간 길이
+        for(int i = 1; i <= N; i++) {
+            int idx = (start + i) % N; // 원형 처리
+            if(arr[idx] == 0) {
                 len++;
-                i++;
+            }else {
+                if(len > 0) {
+                    answer += (len + 1) / 2;
+                    len = 0;
+                }
             }
-            list.add(len);
-        }
-
-        // 원형 처리
-        if(arr[0] == 0 && arr[N - 1] == 0 && list.size() > 1) {
-            int first = list.get(0);
-            int last = list.get(list.size() - 1);
-            list.set(0, first + last);
-            list.remove(list.size() - 1);
-        }
-
-        for(int len : list) {
-            answer += (len + 1) / 2;
         }
 
         System.out.println(answer);
