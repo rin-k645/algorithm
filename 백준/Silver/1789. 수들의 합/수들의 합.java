d716f95a7bf358af3d0bc465ad4930880c8e621a
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2026-04-27 23:41:17 +0900

    [Silver V] Title: 수들의 합, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		long S = Long.parseLong(br.readLine());
+		
+		long sum = 0;
+		long n = 0;
+		while(sum <= S) {
+			n++;
+			sum += n;
+		}
+		
+		System.out.println(n - 1);
+
+	}
+
+}
