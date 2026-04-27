2a206620a171e3620050f4b4d434f3c2ab9b986c
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2026-04-27 23:53:19 +0900

    [Silver I] Title: 포도주 시식, Time: 156 ms, Memory: 15572 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] glass = new int[n + 1];
+		
+		for(int i = 1; i <= n; i++) {
+			glass[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int[] dp = new int[n + 1];
+		
+		dp[1] = glass[1];
+		
+		if(n >= 2) {
+			dp[2] = glass[1] + glass[2];
+		}
+		
+		for(int i = 3; i <= n; i++) {
+			dp[i] = Math.max(dp[i - 1], Math.max(glass[i - 1] + dp[i - 3], dp[i - 2]) + glass[i]);
+		}
+		
+		System.out.println(dp[n]);
+	}
+
+}
