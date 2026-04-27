e92da087f54b480b9a77c418ff23dcdf39d090bb
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2026-04-27 23:28:02 +0900

    [Silver I] Title: 문자열 교환, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.java
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.java
@@ -13,12 +13,15 @@ public class Main {
 			if(str.charAt(i) == 'a') count_a++;
 		}
 		
-		int min = length;
-		for(int i = 0; i < length; i++) {
-			int count_b = 0;
-			for(int j = i; j < i + count_a; j++) {
-				if(str.charAt(j % length) == 'b') count_b++;
-			}
+		int count_b = 0;
+		for(int i = 0; i < count_a; i++) {
+			if(str.charAt(i) == 'b') count_b++;
+		}
+		
+		int min = count_b;
+		for(int i = 1; i < length; i++) {
+			if(str.charAt(i - 1) == 'b') count_b--;
+			if(str.charAt((i + count_a - 1) % length) == 'b') count_b++;
 			min = Math.min(min, count_b);
 		}
 		
