c38654b817f5f907bae16684eae1fb5dbadceb10
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2026-06-21 21:39:33 +0900

    Time: 0 ms (100%), Space: 43.5 MB (95.87%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,33 +1,33 @@
 class Solution {
     public int search(int[] nums, int target) {
-    int left = 0;
-    int right = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
 
-    while(left <= right) {
-        int mid = (left + right) / 2;
+        while(left <= right) {
+            int mid = (left + right) / 2;
 
-        if(nums[mid] == target) {
-            return mid;
-        }
+            if(nums[mid] == target) {
+                return mid;
+            }
 
-        // 왼쪽이 정렬
-        if(nums[left] <= nums[mid]) {
-            if(nums[left] <= target && target < nums[mid]) { // 왼쪽 범위 안에 있음
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+            // 왼쪽이 정렬
+            if(nums[left] <= nums[mid]) {
+                if(nums[left] <= target && target < nums[mid]) { // 왼쪽 범위 안에 있음
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // 오른쪽이 정렬
+            else {
+                if(nums[mid] < target && target <= nums[right]) { // 오른쪽 범위 안에 있음
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
-        // 오른쪽이 정렬
-        else {
-            if(nums[mid] < target && target <= nums[right]) { // 오른쪽 범위 안에 있음
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
+
+        return -1;
     }
-
-    return -1;
-}
 }
