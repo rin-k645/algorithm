c1d0dbdf79bc4989fc9849205e702e8ebdb00c21
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2026-08-15 20:46:46 +0900

    Time: 33 ms (60.92%), Space: 96.2 MB (35.06%) - LeetHub

---
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.java
@@ -0,0 +1,46 @@
+class Solution {
+    private static int[][] grid;
+    private static int[][] dp;
+    private static int m, n;
+    private final int[][] dir = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+
+    public int countPaths(int[][] grid) {
+        this.grid = grid;
+        m = grid.length;
+        n = grid[0].length;
+
+        dp = new int[m][n];
+
+        for(int[] d : dp) {
+            Arrays.fill(d, -1);
+        }
+
+        int answer = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                answer = (answer + dfs(i, j)) % 1_000_000_007;
+            }
+        }
+        return answer;
+    }
+
+    private int dfs(int i, int j) {
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        long sum = 1;
+        for(int[] d : dir) {
+            int ni = i + d[0];
+            int nj = j + d[1];
+
+            if(ni >= 0 && ni < m && nj >= 0 && nj < n) {
+                if(grid[ni][nj] > grid[i][j]) {
+                    sum += dfs(ni, nj);
+                }
+            }
+        }
+        dp[i][j] = (int)(sum % 1_000_000_007);
+        return dp[i][j];
+    }
+}
